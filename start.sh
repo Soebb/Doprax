e4101b54a4d8354dfab2dc5840a4bda54b2d655b
Soebb <87156166+Soebb@users.noreply.github.com>
2024-02-24 16:48:33 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CPPFLAGS=-I/usr/include/python3.10/Python.h
+export CPPFLAGS=-I/usr/include/python3.10
 git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
 $START_CMD
